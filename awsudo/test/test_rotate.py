3219767cbdba4c56975f65ee3716d965ceec8422
Cell <>
2020-04-03 00:55:06 +0200

    REMOVE tests of awsrotate

--- awsudo/test/test_rotate.py
+++
@@ -1,6 +0,0 @@
-def test_entrypoint():
-    '''This is a simple enough program that some lazy developer didn't bother
-    writing tests. However, importing it will reveal some issues.
-    '''
-    from awsudo import rotate
-    assert hasattr(rotate, 'main')
