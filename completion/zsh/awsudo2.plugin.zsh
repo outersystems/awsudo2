9ead23331cab70c212003cc1c0534aa90a0238ba
Cell <>
2020-05-01 00:46:24 +0200

    REFACTOR awsudo2.plugin.zsh

--- completion/zsh/awsudo2.plugin.zsh
+++ completion/zsh/awsudo2.plugin.zsh
@@ -12,38 +12,48 @@ _awsudo2() {
   typeset -A opt_args
 
   if [[ -v AWS_PROFILE ]]; then
-    _arguments -C '1:targets: _command_names' \
-                  '*:: :->target' \
-                  && ret=0
+    _arguments -C \
+      '1:targets: _command_names' \
+      '*:: :->target' \
+    && ret=0
   else
-    _arguments -C '1:subcommands:->cmds' \
-                  '2:profiles:->profile_list' \
-                  '3:targets: _command_names' \
-                  '*:: :->target' \
-                  && ret=0
+    _arguments -C \
+      '1:subcommands:->cmds' \
+      '2:profiles:->profile_list' \
+      '3:targets: _command_names' \
+      '*:: :->target' \
+    && ret=0
   fi
 
   case "$state" in
-  (cmds)
-    local commands; commands=(
-      '-u: AWS Profile'
-    )
-
-    _describe -t commands 'command' commands && ret=0
-  ;;
-  (profile_list)
-    local profiles; profiles=(
-      $(grep "\[" ~/.aws/config | sed 's#^.profile \(.*\)\]#\1#')
-    )
-    _describe -t profiles 'profiles' profiles && ret=0
-  ;;
-  (target)
-    ((CURRENT--))
-    shift words
-    shift words
-    echo $words | sudo tee blah.log
-    _complete && ret=0
-  ;;
+    (cmds)
+      local commands;
+      commands=(
+        '-u: AWS Profile'
+      )
+       _describe -t commands 'command' commands && ret=0
+      ;;
+    (profile_list)
+      local profiles;
+      profiles=(
+        $(cat ~/.aws/config \
+          | awk '
+            /BEGIN/ { profile = ""; desc = "" }
+            /^\[profile/ { profile=$0 ; gsub("\[profile ", "", profile); gsub("\]", "", profile) }
+            /^role_arn/ { desc=$0; gsub("role_arn = ", "", desc) }
+            /^$/ { printf("%s:%s\n", profile, desc); profile = ""; desc = ""}
+            /END/ { printf("%s:%s\n", profile, desc) }' \
+          | sort -u
+        )
+      )
+      _describe -t profiles 'profiles' profiles && ret=0
+     ;;
+    (target)
+      ((CURRENT--))
+      shift words
+      shift words
+      _complete && ret=0
+      ;;
   esac
 
   return $ret
